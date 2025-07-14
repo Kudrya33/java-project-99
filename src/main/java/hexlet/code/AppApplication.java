580b36f56ef8197ddc895e690300343e01834efa
Павел Кудря <123943120+Kudrya33@users.noreply.github.com>
2025-07-14 19:15:15 +0300

    Update AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public Faker getFaker() {
-        return new Faker(new Locale("en", "US"));
     }
 }
