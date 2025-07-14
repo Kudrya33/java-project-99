bfcc7ebb70dcc43936f8ef5120f8f6da17297169
Павел Кудря <Skelger@mail.ru>
2025-07-14 18:26:45 +0300

    add new project

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+	application
+	jacoco
+	checkstyle
+	id("java")
+	id("org.springframework.boot") version "4.0.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("io.freefair.lombok") version "8.12.1"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+	//implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	runtimeOnly("com.h2database:h2")
+	implementation("org.postgresql:postgresql:42.7.3")
+
+	//implementation("net.datafaker:datafaker:2.0.2")
+	implementation("org.instancio:instancio-junit:3.3.1")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+	//testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+	reports { xml.required.set(true) }
+}
